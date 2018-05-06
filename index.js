b36154afbf2c8af2a6d78ce641daf0c8843d03a3
Santeri Rusila <santtu_1993@hotmail.com>
2018-05-06 13:55:07 +0300

    Sequalize models

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 export const schema = makeExecutableSchema({
   typeDefs,
@@ -21,4 +22,6 @@ app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-app.listen(PORT);
+models.sequelize.sync({}).then(() => {
+  app.listen(PORT);
+});
